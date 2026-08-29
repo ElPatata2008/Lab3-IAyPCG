ef9a5e90f423e7b9b24a62a609145342d60762c9
Peter <pedrojavier.morales13@gmail.com>
2026-08-29 15:58:53 -0400

    Finished Pacman AI
    Sólo es capaz de escapar y perseguir a los fantasma en relación si son comestibles o no.
    
    Debido a que Pacman puede retroceder, tiende a quedarse en una esquina, ya que siempre la posición a la que se mueve y la que estaba son las más lejanas al fantasma más cercano, pero escapa en el momento en el que el fantasma se acerca escapa normalmente. Como los fantasmas empiezan arriba de Pacman, el mismo siempre tiende a ir abajo al comienzo de la partida, pero si el fantasma más cercano, de alguna forma, llega abajo de Pacman, subirá.

--- src/PacmanDTController.cpp
+++ src/PacmanDTController.cpp
@@ -1,4 +1,7 @@
 #include "PacmanDTController.h"
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <cmath>
 
 PacmanDTController::PacmanDTController(std::shared_ptr<Character> character): Controller(character) {}
 
@@ -10,36 +13,101 @@ Move PacmanDTController::getMove(const GameState& game) {
         return RIGHT;
     }
 
-    std::vector<int> ghostsNodes;
-    std::vector<std::pair<int, int>> ghostsCoords;
+    SDL_Event e;
+	if( SDL_PollEvent( &e ) != 0 )
+	{
+		if( e.type == SDL_QUIT || 
+			(e.type == SDL_KEYDOWN && 
+				(e.key.keysym.sym==SDLK_ESCAPE || 
+				e.key.keysym.sym==SDLK_q) ))
+		{
+			SDL_Quit();
+			exit(0);
+		}
+	}
+
+    int ghostDist = 10000000;
+    int ghostNum;
+    int ghostNode;
+    std::pair<int, int> ghostCoord;
+    
+    int pacmanNode = character->getPos();
+    auto pacmanCoord = game.getMaze().getNodePos(pacmanNode);
+
 
     for (int i = 0; i < 4; i++) {
-        ghostsNodes[i] = game.getGhostsPos(i);
-        ghostsCoords[i] = game.getMaze().getNodePos(ghostsNodes[i]);
+        int node = game.getGhostsPos(i);
+        auto coords = game.getMaze().getNodePos(node);
+
+        // coords.first -= pacmanCoord.first;
+        // coords.second -= pacmanCoord.second;
+
+        int sqX = std::pow(coords.first - pacmanCoord.first, 2);
+        int sqY = std::pow(coords.second - pacmanCoord.second, 2);
+        int sqDist = std::sqrt(sqX + sqY);
+
+         if (sqDist < ghostDist) {
+            ghostDist = sqDist;
+            ghostNode = node;
+            ghostCoord = coords;
+            ghostNum = i;
+         }
+
     }
 
-    int pacmanNode = character->getPos();
+    std::cout << "Ghost Distance: " << ghostDist <<
+                ", Ghost Node: " << ghostNode <<
+                ", Ghost Coords: " << ghostCoord.first << ", " << ghostCoord.second <<
+                ", Ghost Num: " << ghostNum << std::endl;
 
-    Ghost *ghost = dynamic_cast<Ghost*>(character.get());
-    if (!ghost->isEdible()) {
+    if (game.isGhostEdible(ghostNum)) {
         int minDist = 10000000;
         Move minMove;
-        std::vector<Move> moves = game.getMaze().getPossibleMoves(character->getPos());
+        std::vector<Move> moves = game.getMaze().getPossibleMoves(pacmanNode);
 
         for (Move m : moves) {
             int neighbor = game.getMaze().getNeighbour(pacmanNode, m);
-
             if (neighbor < 0) continue;
-
             auto neighborCoords = game.getMaze().getNodePos(neighbor);
 
+            // neighborCoords.first -= ghostCoord.first;
+            // neighborCoords.second -= ghostCoord.second;
+
+            int sqX = std::pow(neighborCoords.first - ghostCoord.first, 2);
+            int sqY = std::pow(neighborCoords.second - ghostCoord.second, 2);
+            int sqDist = std::sqrt(sqX + sqY);
+
+            if (sqDist < minDist) {
+                minDist = sqDist;
+                minMove = m;
+                // std::cout << "Best Move: " << m << std::endl;
+            }
         }
+
 
         return minMove;
     } else {
         int maxDist = -1;
         Move maxMove;
-        std::vector<Move> moves = game.getMaze().getPossibleMoves(character->getPos());
+        std::vector<Move> moves = game.getMaze().getPossibleMoves(pacmanNode);
+
+        for (Move m : moves) {
+            int neighbor = game.getMaze().getNeighbour(pacmanNode, m);
+            if (neighbor < 0) continue;
+            auto neighborCoords = game.getMaze().getNodePos(neighbor);
+
+            // neighborCoords.first -= ghostCoord.first;
+            // neighborCoords.second -= ghostCoord.second;
+
+            int sqX = std::pow(neighborCoords.first - ghostCoord.first, 2);
+            int sqY = std::pow(neighborCoords.second - ghostCoord.second, 2);
+            int sqDist = std::sqrt(sqX + sqY);
+
+            if (sqDist > maxDist) {
+                maxDist = sqDist;
+                maxMove = m;
+            }
+        }
 
         return maxMove;
     }
--- src/PacmanDTController.h
+++ src/PacmanDTController.h
@@ -4,6 +4,8 @@
 #include "Controller.h"
 
 class PacmanDTController : public Controller {
+
+public: 
     PacmanDTController(std::shared_ptr<Character> character);
     virtual ~PacmanDTController();
     virtual Move getMove(const GameState& game) override;
